e345977d2e910b2064e93dcc0c5629b94e9c2d01
Doug Swisher <big.swish@gmail.com>
2017-08-12 10:50:36 -0500

    DNA, refined

--- dna/dna.py
+++ dna/dna.py
@@ -4,16 +4,6 @@ if len(sys.argv) < 2:
     print 'You must specify the name of the file to load.'
     sys.exit(1)
 
-fname = sys.argv[1]
-f = open(fname, 'r')
-dna = f.read()
-f.close()
-num = {}
+s = file(sys.argv[1]).read()
+print s.count("A"), s.count("G"), s.count("C"), s.count("T")
 
-for c in dna:
-    if c in num:
-        num[c] += 1
-    else:
-        num[c] = 1
-
-print '{} {} {} {}'.format(num["A"], num["C"], num["G"], num["T"])
