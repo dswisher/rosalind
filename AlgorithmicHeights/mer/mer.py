5ad956a11d7f0505f7aab1c4350b324b4905ba7f
Doug Swisher <big.swish@gmail.com>
2017-09-02 16:11:44 -0500

    MER, Python, first version

---
+++ AlgorithmicHeights/mer/mer.py
@@ -0,0 +1,40 @@
+
+import sys
+
+if len(sys.argv) != 2:
+    print "Please enter the name of the data file!"
+    sys.exit(1)
+
+with open(sys.argv[1], "r") as fp:
+    n = int(fp.readline())
+    A = map(int, fp.readline().split())
+    m = int(fp.readline())
+    B = map(int, fp.readline().split())
+
+
+def merge(a, b):
+    i = 0
+    j = 0
+    k = 0
+    result = [0] * (len(a) + len(b))
+    while i < len(a) or j < len(b):
+        if i < len(a):
+            if j < len(b):
+                if a[i] < b[j]:
+                    result[k] = a[i]
+                    i += 1
+                else:
+                    result[k] = b[j]
+                    j += 1
+            else:
+                result[k] = a[i]
+                i += 1
+        else:
+            result[k] = b[j]
+            j += 1
+        k += 1
+    return result
+
+
+x = map(str, merge(A, B))
+print " ".join(x)
