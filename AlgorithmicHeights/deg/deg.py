12fff4c7d4e8dca0808ba0bc857217e619103af2
Doug Swisher <big.swish@gmail.com>
2017-09-05 07:26:15 -0500

    DEG, Python, first version

---
+++ AlgorithmicHeights/deg/deg.py
@@ -0,0 +1,21 @@
+
+import sys
+
+if len(sys.argv) != 2:
+    print "Please enter the name of the data file to load!"
+    sys.exit(1)
+
+nums = []
+with open(sys.argv[1], "r") as fp:
+    n, m = map(int, fp.readline().split())
+    for line in fp.readlines():
+        a, b = map(int, line.split())
+        nums.append(a)
+        nums.append(b)
+
+deg = [0] * n
+
+for i in xrange(len(nums)):
+    deg[nums[i] - 1] += 1
+
+print " ".join(map(str, deg))
