b15fcaa093d83f61cf1706f47ab9c87429dfc903
Doug Swisher <big.swish@gmail.com>
2017-09-13 07:20:58 -0500

    Move TREE to new directory structure

--- rosalind/bioinformatics/stronghold/tree/main.py
+++ rosalind/bioinformatics/stronghold/tree/main.py
@@ -1,15 +1,15 @@
 
 import sys
+from rosalind.common import util
 
 if len(sys.argv) != 2:
     print "You must enter the name of the file to load!"
     sys.exit(1)
 
-with open(sys.argv[1], "r") as file:
+with open(util.find_file(sys.argv[1]), "r") as file:
     num = int(file.readline())
     edges = 0
     while file.readline():
         edges += 1
 
 print num - edges - 1
-
