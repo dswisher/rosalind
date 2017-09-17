3d7dcde2ca3dff66fe60249d69ccbf3c7f468349
Doug Swisher <big.swish@gmail.com>
2017-09-17 10:18:49 -0500

    Checkpoint2: LONG

--- rosalind/bioinformatics/stronghold/long/main.py
+++ rosalind/bioinformatics/stronghold/long/main.py
@@ -4,15 +4,40 @@ from rosalind.common import util
 from rosalind.bioinformatics.common import fasta
 
 
-# https://stackoverflow.com/a/2894073/282725
+def max_overlap(s1, s2):
+    mlap = 0
+    for k in xrange(1, min(len(s1), len(s2))):
+        if s1[-k:] == s2[:k]:
+            mlap = k
+        else:
+            return mlap
+    return mlap
+
+
+def find_max_overlap(seqs, pos):
+    max_olap = 0
+    max_i = -1
+    for i in xrange(len(seqs)):
+        if i != pos:
+            olap = max_overlap(seqs[pos], seqs[i])
+            if olap > max_olap:
+                max_olap = olap
+                max_i = i
+    return max_i
+
+
 def find_shortest_superstring(seqs):
+    pos = 0
+    while len(seqs) > 1:
+        olap = find_max_overlap(seqs, pos)
+        print pos, olap
+        break
     # TODO
     return "FRED"
 
 
 def main(fname):
     seqs, _ = fasta.read(util.find_file(fname))
-    print (seqs)
     print (find_shortest_superstring(seqs))
 
 
--- rosalind/bioinformatics/stronghold/long/test_main.py
+++ rosalind/bioinformatics/stronghold/long/test_main.py
@@ -5,6 +5,9 @@ from . import main
 
 class TestShortestSuperstring(unittest.TestCase):
 
+    def test_max_overlap(self):
+        self.assertEquals()
+
     def test_sample1(self):
         seqs = ["ATTAGACCTG", "CCTGCCGGAA", "AGACCTGCCG", "GCCGGAATAC"]
         sup = main.find_shortest_superstring(seqs)
