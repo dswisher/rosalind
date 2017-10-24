2ef03d4da42bc12c1b3c3241440943b615e5eaf2
Doug Swisher <big.swish@gmail.com>
2017-10-24 07:35:46 -0500

    Checkpoint 2: CORR

--- rosalind/bioinformatics/stronghold/corr/main.py
+++ rosalind/bioinformatics/stronghold/corr/main.py
@@ -2,18 +2,41 @@
 import sys
 from rosalind.common import util
 from rosalind.bioinformatics.common import fasta
+from rosalind.bioinformatics.common import kmers
+
+
+def build_counts(seqs):
+    comps = dict()
+    for s in seqs:
+        if s in comps:
+            comps[s] += 1
+        else:
+            comps[s] = 1
+        comp = kmers.reverse_complement(s)
+        if comp in comps:
+            comps[comp] += 1
+        else:
+            comps[comp] = 2
+    return comps
 
 
 def find_corrections(seqs):
-    # TODO
-    return ["TBD"]
-    # return ["TTCAT->TTGAT", "GAGGA->GATGA", "TTTCC->TTTCA"]
+    corrs = []
+    comps = build_counts(seqs)
+    # print comps
+    for s1, num in comps.iteritems():
+        if num == 1:
+            for s2 in comps:
+                if kmers.hamming_distance(s1, s2) == 1:
+                    corrs.append(s1 + "->" + s2)
+    return corrs
 
 
 def main(fname):
     seqs, _ = fasta.read(util.find_file(fname))
     corrections = find_corrections(seqs)
-    print corrections
+    for c in corrections:
+        print c
 
 
 if __name__ == '__main__':
